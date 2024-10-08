220a54a403b211b5792fafca6210e8a9ac7027c4
liqilei <blighli@gmail.com>
2024-10-09 02:07:40 +0800

    add menubar statusbar and layout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from argparse import Action
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox, QMenu, QHBoxLayout, QVBoxLayout, \
+    QWidget, QLCDNumber, QLineEdit
 from PyQt6.QtGui import QAction
 
 class MainWindow(QMainWindow):
@@ -10,6 +11,21 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(okButton)
+        hbox.addWidget(cancelButton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        central_widget = QWidget()
+        central_widget.setLayout(vbox)
+        self.setCentralWidget(central_widget)
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('File')
@@ -17,18 +33,32 @@ class MainWindow(QMainWindow):
         fileMenu.addAction(newAct)
         newAct.triggered.connect(self.on_button_clicked)
 
+        self.toolbar = self.addToolBar('Toolbar')
+        self.toolbar.addAction(newAct)
+
         self.statusBar().showMessage('Ready')
 
-        self.setGeometry(300, 300, 1200, 800)
+        self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle("PyQt6 应用程序示例")  # 设置窗口标题
         # 创建按钮
         button = QPushButton("Click me!", self)
         button.setGeometry(300, 200, 200, 50)  # 设置按钮的位置和大小
         # 将按钮点击事件与处理函数关联
         button.clicked.connect(self.on_button_clicked)
+
+        self.lcd = QLCDNumber(central_widget)
         # 显示窗口
         self.center()
         self.show()
+
+    def contextMenuEvent(self, event):
+        cmenu = QMenu(self)
+        newAct = cmenu.addAction('New')
+        openAct = cmenu.addAction('Open')
+        quitAct = cmenu.addAction('Quit')
+        action = cmenu.exec(self.mapToGlobal(event.pos()))
+        if action == quitAct:
+            QApplication.instance().quit()
 
     def center(self):
         qr = self.frameGeometry()
@@ -39,9 +69,10 @@ class MainWindow(QMainWindow):
 
     # 按钮点击事件的处理函数
     def on_button_clicked(self):
+        self.lcd.display(889)
         message_box = QMessageBox()
         message_box.setText("Hello, PyQt6!")  # 创建一个消息框并显示消息
-        message_box.exec()
+        # message_box.exec()
 
 
 
