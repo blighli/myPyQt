fa6d2f5d282d6ff003a846c346288b8fe72916a5
liqilei <blighli@gmail.com>
2024-10-09 01:28:21 +0800

    add menu and status bar to main window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
+from argparse import Action
+
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox
+from PyQt6.QtGui import QAction
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,13 +10,22 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+        newAct = QAction('New', self)
+        fileMenu.addAction(newAct)
+        newAct.triggered.connect(self.on_button_clicked)
+
+        self.statusBar().showMessage('Ready')
+
         self.setGeometry(300, 300, 400, 300)
         self.setWindowTitle("PyQt6 应用程序示例")  # 设置窗口标题
         # 创建按钮
         button = QPushButton("Click me!", self)
         button.setGeometry(50, 50, 100, 30)  # 设置按钮的位置和大小
         # 将按钮点击事件与处理函数关联
-        button.clicked.connect(on_button_clicked)
+        button.clicked.connect(self.on_button_clicked)
         # 显示窗口
         self.center()
         self.show()
@@ -25,11 +37,13 @@ class MainWindow(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-# 按钮点击事件的处理函数
-def on_button_clicked():
-    message_box = QMessageBox()
-    message_box.setText("Hello, PyQt6!")  # 创建一个消息框并显示消息
-    message_box.exec()
+    # 按钮点击事件的处理函数
+    def on_button_clicked(self):
+        message_box = QMessageBox()
+        message_box.setText("Hello, PyQt6!")  # 创建一个消息框并显示消息
+        message_box.exec()
+
+
 
 def main():
     # 创建应用程序对象
